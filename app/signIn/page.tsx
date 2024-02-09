8b0d4b7dbc9d4aed44b48be4ae59b9bf25fa5dc6
MaxAnii <ansarbaba2000@gmail.com>
2024-02-09 21:38:28 +0530

    added a signin page.

---
+++ app/signIn/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import FormContainer from "@/components/auth/formContainer";
+import LoginForm from "@/components/auth/loginForm";
+
+const SignIn = () => {
+	return (
+		<div>
+			<FormContainer
+				title="Welcom back"
+				backButtonLabel="Don't have an account?"
+				backButtonhref="/signUp"
+			>
+				<LoginForm></LoginForm>
+			</FormContainer>
+		</div>
+	);
+};
+
+export default SignIn;
