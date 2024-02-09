60191b44a44b6b75aa1ef3c3e998710c764c496c
MaxAnii <ansarbaba2000@gmail.com>
2024-02-09 21:39:40 +0530

    Theme toggel buttons.

--- components/themeToggel.tsx
+++ components/themeToggel.tsx
@@ -1,20 +1,35 @@
-"use client"
-import { useTheme } from "next-themes"
-
+"use client";
+import { useTheme } from "next-themes";
+import { MdDarkMode } from "react-icons/md";
+import { CiLight } from "react-icons/ci";
+import { useState } from "react";
 const ThemeToggel = () => {
-    const {setTheme} = useTheme()
-  return (
-    <div>
-      
-        <button className=" py-2 px-3" 
-        onClick={()=>setTheme("light")}>
-        Light
-        </button>
-        <button onClick={()=>setTheme("dark")} >
-        Dark
-        </button >
-        </div>
-  )
-}
+	const { setTheme } = useTheme();
+	const [toggel, setToggel] = useState<Boolean>(false);
+	return (
+		<div>
+			{toggel ? (
+				<button
+					className=" py-2 px-3"
+					onClick={() => {
+						setTheme("light");
+						setToggel(!toggel);
+					}}
+				>
+					<CiLight size={15}></CiLight>
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						setTheme("dark");
+						setToggel(!toggel);
+					}}
+				>
+					<MdDarkMode size={20}></MdDarkMode>
+				</button>
+			)}
+		</div>
+	);
+};
 
-export default ThemeToggel
+export default ThemeToggel;
