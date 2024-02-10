1cb6b8878d1589f1ac967003b5af98b96fd0652f
MaxAnii <ansarbaba2000@gmail.com>
2024-02-10 23:28:25 +0530

    created dashboard dashbar.

---
+++ components/dashNav.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import NavLinks from "./navLinks";
+
+const DashNav = () => {
+	return (
+		<div>
+			<NavBar>
+				<NavLinks></NavLinks>
+			</NavBar>
+		</div>
+	);
+};
+
+export default DashNav;
