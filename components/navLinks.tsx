773e14780bd0fc04e07db560972a489af020074b
MaxAnii <ansarbaba2000@gmail.com>
2024-02-10 23:29:49 +0530

    added a components to navigate in authRoutes.

---
+++ components/navLinks.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import SignOut from "./signOut";
+
+const NavLinks = () => {
+	const routes = [
+		{
+			label: "Dashboard",
+			href: "/dashboard",
+		},
+		{
+			label: "Home",
+			href: "/dashboard",
+		},
+	];
+	return (
+		<div>
+			{routes.map((item, index) => {
+				return (
+					<Link
+						href={item.href}
+						key={index}
+						className="p-2 text-sm bg-accent rounded-lg ml-2"
+					>
+						{item.label}
+					</Link>
+				);
+			})}
+			<SignOut></SignOut>
+		</div>
+	);
+};
+export default NavLinks;
