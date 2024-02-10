23fe75f9553bf97c6714b6278870e79fd07ac90b
MaxAnii <ansarbaba2000@gmail.com>
2024-02-10 23:29:23 +0530

    created a layout for the navbar.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,10 @@
+"use client";
 import ThemeToggel from "./themeToggel";
-import {
-	Menubar,
-	MenubarContent,
-	MenubarItem,
-	MenubarMenu,
-	MenubarSeparator,
-	MenubarShortcut,
-	MenubarTrigger,
-} from "@/components/ui/menubar";
-import { Button } from "./ui/button";
-import Link from "next/link";
 
-const NavBar = () => {
+import Link from "next/link";
+import AuthLinks from "./authLinks";
+
+const NavBar = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="">
 			<header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full  text-sm py-4">
@@ -23,13 +16,7 @@ const NavBar = () => {
 						SENTRY
 					</Link>
 					<div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
-						<Button className="font-medium" size="sm">
-							<Link href="/signIn">Sign In</Link>
-						</Button>
-						<Button className="font-medium" size="sm" variant="secondary">
-							<Link href="/signUp">Sign Up</Link>
-						</Button>
-
+						{children}
 						<ThemeToggel></ThemeToggel>
 					</div>
 				</nav>
