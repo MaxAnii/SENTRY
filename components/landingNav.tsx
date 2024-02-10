5648e4d71e0bfc201a7118c97404032710b96826
MaxAnii <ansarbaba2000@gmail.com>
2024-02-10 23:28:54 +0530

    changed the layout of landing Navbar.

---
+++ components/landingNav.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import AuthLinks from "./authLinks";
+
+const LandingNav = () => {
+	return (
+		<div>
+			<NavBar>
+				<AuthLinks></AuthLinks>
+			</NavBar>
+		</div>
+	);
+};
+
+export default LandingNav;
