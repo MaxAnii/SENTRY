d36b64aa354bded0579cbde8a9040d18e794abde
MaxAnii <ansarbaba2000@gmail.com>
2024-02-10 23:27:23 +0530

    added a landing navbar.

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,9 +1,10 @@
 import FormContainer from "@/components/auth/formContainer";
 import RegisterForm from "@/components/auth/registerForm";
-
+import LandingNav from "@/components/landingNav";
 const SignUp = () => {
 	return (
 		<div>
+			<LandingNav></LandingNav>
 			<FormContainer
 				title="Create an account"
 				backButtonLabel="Already have an account?"
