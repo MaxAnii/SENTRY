fc828ef6b73c36b3197a5997c961d459779044f9
MaxAnii <ansarbaba2000@gmail.com>
2024-02-11 23:32:53 +0530

    changed the css style.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,13 +9,15 @@ const NavBar = ({ children }: { children: React.ReactNode }) => {
 		<div className="">
 			<header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full  text-sm py-4">
 				<nav
-					className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+					className="max-w-[85rem] w-full mx-auto px-4 flex "
 					aria-label="Global"
 				>
-					<Link className="flex-none text-xl font-semibold " href="/">
-						SENTRY
-					</Link>
-					<div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
+					<div>
+						<Link className="flex-none text-xl font-semibold " href="/">
+							SENTRY
+						</Link>
+					</div>
+					<div className="ml-auto mr-11 flex gap-x-3">
 						{children}
 						<ThemeToggel></ThemeToggel>
 					</div>
