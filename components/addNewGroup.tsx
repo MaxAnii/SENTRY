7946042ea6456d9f21bcbc3e8dee1d792869aad5
MaxAnii <ansarbaba2000@gmail.com>
2024-02-11 23:31:45 +0530

    Added modal where user can added new group.

---
+++ components/addNewGroup.tsx
@@ -0,0 +1,45 @@
+"use client";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
+import NewGroupForm from "./newGroupForm";
+const AddNewGroup = () => {
+	const [isClient, setIsClient] = useState<Boolean>(false);
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+	return (
+		<div>
+			{isClient && (
+				<AlertDialog>
+					<AlertDialogTrigger>
+						<Button>Add a new group</Button>
+					</AlertDialogTrigger>
+					<AlertDialogContent>
+						<AlertDialogHeader>
+							<AlertDialogTitle>Add new group</AlertDialogTitle>
+							<AlertDialogDescription>
+								<NewGroupForm></NewGroupForm>
+							</AlertDialogDescription>
+						</AlertDialogHeader>
+						<AlertDialogFooter>
+							<AlertDialogCancel>Cancel</AlertDialogCancel>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialog>
+			)}
+		</div>
+	);
+};
+
+export default AddNewGroup;
