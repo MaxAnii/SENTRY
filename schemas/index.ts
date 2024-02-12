fe77cb208b6364684d26669213c5223f45559e4b
MaxAnii <ansarbaba2000@gmail.com>
2024-02-12 23:22:16 +0530

    removed the confim password.

--- schemas/index.ts
+++ schemas/index.ts
@@ -10,9 +10,6 @@ export const registerSchema = z.object({
     email:z.string().email(),
     password:z.string().min(8),
     confrimPassword:z.string().min(8)
-}).refine((data)=>data.confrimPassword === data.password,{
-    message:"Password don't match",
-    path:["confirm"]
 })
 
 
