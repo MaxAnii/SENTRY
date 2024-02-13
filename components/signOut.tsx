5a32d734307334288ce37a1a6edff08dcd2d4542
MaxAnii <ansarbaba2000@gmail.com>
2024-02-13 23:19:04 +0530

    use the next auth to signOut.

--- components/signOut.tsx
+++ components/signOut.tsx
@@ -1,18 +1,18 @@
-"use client";
+import { signOut } from "@/auth";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
+
 const SignOut = () => {
-	const rout = useRouter();
 	return (
-		<Button
-			className="p-2 text-sm ml-2"
-			onClick={() => {
-				rout.push("/", { scroll: false });
-				localStorage.removeItem("login");
+		<form
+			action={async () => {
+				"use server";
+				await signOut();
 			}}
 		>
-			Logout
-		</Button>
+			<Button className="p-2 text-sm ml-2" type="submit">
+				Logout
+			</Button>
+		</form>
 	);
 };
 
