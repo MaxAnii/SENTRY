f3371d23f8e932b17c125ef6d3046bb172f3ce0c
MaxAnii <ansarbaba2000@gmail.com>
2024-02-13 23:14:05 +0530

    added signIn credentails to authenticate the user.

--- actions/login.ts
+++ actions/login.ts
@@ -2,19 +2,36 @@
 import { loginSchema } from "@/schemas"
 import * as z from "zod"
 import { db } from "@/lib/db"
-import bcrypt from "bcrypt"
+import { signIn } from "@/auth"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+import { AuthError } from "next-auth"
+
+
 export const login=async(values:z.infer<typeof loginSchema>)=>{
 const validateFields = loginSchema.safeParse(values)
 if(!validateFields.success) return {error:"no match"}
 const {email,password} = validateFields.data;
-
-const hashedPassword = await bcrypt.hash(password, 10);
-const userExits =await  db.user.findUnique({
-    where:{
-        email,
-        password: hashedPassword,
+try {
+    await signIn("credentials",{
+        email,password,
+        redirectTo: DEFAULT_LOGIN_REDIRECT 
+ 
+    })
+} catch (error) {
+    if (error instanceof AuthError){
+        console.log(error.message)
+        switch(error.type){
+            case "CredentialsSignin":
+                return {
+                    error:"Invalid credentials"
+                }
+            default:
+                return {
+                    error:"something went wrong"
+                }
+        }
     }
-})
-if(!userExits) return {error:"Credentails does not match"}
-return {success:"Auth"}
+    throw error
+}
+
 }
