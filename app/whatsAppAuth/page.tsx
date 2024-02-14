022dafb2b3202634bdc0abeef6f2dea1dbe4cef2
MaxAnii <ansarbaba2000@gmail.com>
2024-02-14 19:58:33 +0530

    added whatsAppAuth page.

---
+++ app/whatsAppAuth/page.tsx
@@ -0,0 +1,13 @@
+import LandingNav from "@/components/landingNav";
+import WhatAppVerificationForm from "@/components/auth/whatAppVerificationForm";
+
+const page = () => {
+	return (
+		<div>
+			<LandingNav></LandingNav>
+			<WhatAppVerificationForm></WhatAppVerificationForm>
+		</div>
+	);
+};
+
+export default page;
