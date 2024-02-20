1a50f6ce96dd0b530189cf9ee2d13f15189f34d4
MaxAnii <ansarbaba2000@gmail.com>
2024-02-20 22:23:06 +0530

    Added dropdown menu.

--- components/account.tsx
+++ components/account.tsx
@@ -1,5 +1,42 @@
-const account = () => {
-	return <div>account</div>;
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import SignOut from "./signOut";
+import Link from "next/link";
+
+const Account = () => {
+	return (
+		<div>
+			<DropdownMenu>
+				<DropdownMenuTrigger>
+					<div>
+						<img
+							src="https://github.com/shadcn.png"
+							className="h-9 w-9 rounded-lg"
+						/>
+					</div>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuLabel>My Account</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<Link href="/account">Profile</Link>
+					</DropdownMenuItem>
+
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignOut></SignOut>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
 };
 
-export default account;
+export default Account;
