f90d050f5e438c0f9477adbc5318cf5cd637127c
MaxAnii <ansarbaba2000@gmail.com>
2024-02-20 22:24:18 +0530

    Added account component.

--- components/dashNav.tsx
+++ components/dashNav.tsx
@@ -1,11 +1,15 @@
 import NavBar from "./NavBar";
+import Account from "./account";
 import NavLinks from "./navLinks";
 
 const DashNav = () => {
 	return (
 		<div>
 			<NavBar>
-				<NavLinks></NavLinks>
+				<>
+					<NavLinks></NavLinks>
+					<Account></Account>
+				</>
 			</NavBar>
 		</div>
 	);
