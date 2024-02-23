7329fa7005cd575b0e8b7f2a8e929bb5f97ed311
MaxAnii <ansarbaba2000@gmail.com>
2024-02-23 23:08:24 +0530

    Check the email is verified or not.

--- actions/login.ts
+++ actions/login.ts
@@ -5,12 +5,24 @@ import { db } from "@/lib/db"
 import { signIn } from "@/auth"
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 import { AuthError } from "next-auth"
+import { generateVerificationToken } from "@/lib/tokens"
 
 
 export const login=async(values:z.infer<typeof loginSchema>)=>{
 const validateFields = loginSchema.safeParse(values)
 if(!validateFields.success) return {error:"no match"}
 const {email,password} = validateFields.data;
+const existingUser = await db.user.findFirst({
+    where:{email}
+})
+if(! existingUser || !existingUser.email || !existingUser.password ){
+    return {error:"Email does not exist!"}
+}
+if(!existingUser.emailVerified){
+    const verficationToken = await generateVerificationToken(existingUser.email)
+   return {success:"Confrimation Email Sent!"}
+}
+
 try {
     await signIn("credentials",{
         email,password,
