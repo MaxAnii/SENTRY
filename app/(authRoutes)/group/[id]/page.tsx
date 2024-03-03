57c0ead0453ee5e1bf0f21b97f88d4c9ea48030a
MaxAnii <ansarbaba2000@gmail.com>
2024-03-03 23:59:55 +0530

    Removed console statements.

--- app/(authRoutes)/group/[id]/page.tsx
+++ app/(authRoutes)/group/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { useParams } from "next/navigation";
 const page = () => {
 	const params = useParams<{ id: string }>();
-	console.log(params.id);
+
 	return <div>asbvsfdgb gnasdg kn skadg sa</div>;
 };
 
