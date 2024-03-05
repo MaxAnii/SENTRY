c76d1096bcca542bcadd2ae6950ab6c47bfd954d
MaxAnii <ansarbaba2000@gmail.com>
2024-03-05 23:54:47 +0530

    Added a ui when no group is created.

--- components/dashTable.tsx
+++ components/dashTable.tsx
@@ -11,40 +11,45 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useRouter } from "next/navigation";
 import data from "../tempData/dashboard.json";
+import NoTableScreen from "@/components/NoTableScreen";
 const DashTable = () => {
 	const route = useRouter();
 
 	return (
 		<div>
-			<Table>
-				<TableCaption>A list of your Groups</TableCaption>
-				<TableHeader>
-					<TableRow>
-						<TableHead className="w-[100px] p-4">Icon</TableHead>
-						<TableHead className=" p-4">Name</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{data.map((item, index) => {
-						return (
-							<TableRow
-								onClick={() => {
-									route.push(`/group/${item.id}`);
-								}}
-								key={index}
-							>
-								<TableCell className="font-medium p-4">
-									<Avatar>
-										<AvatarImage src="https://github.com/shadcn.png" />
-										<AvatarFallback>CN</AvatarFallback>
-									</Avatar>
-								</TableCell>
-								<TableCell className=" pl-4">{item.groupName}</TableCell>
-							</TableRow>
-						);
-					})}
-				</TableBody>
-			</Table>
+			{!data ? (
+				<Table>
+					<TableCaption>A list of your Groups</TableCaption>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="w-[100px] p-4">Icon</TableHead>
+							<TableHead className=" p-4">Name</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{/* {data.map((item, index) => {
+							return (
+								<TableRow
+									onClick={() => {
+										route.push(`/group/${item.id}`);
+									}}
+									key={index}
+								>
+									<TableCell className="font-medium p-4">
+										<Avatar>
+											<AvatarImage src="https://github.com/shadcn.png" />
+											<AvatarFallback>CN</AvatarFallback>
+										</Avatar>
+									</TableCell>
+									<TableCell className=" pl-4">{item.groupName}</TableCell>
+								</TableRow>
+							);
+						})} */}
+					</TableBody>
+				</Table>
+			) : (
+				<NoTableScreen></NoTableScreen>
+			)}
 		</div>
 	);
 };
