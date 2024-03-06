b575ddcac7fb8da28241738e92cf20df552fe0fe
MaxAnii <ansarbaba2000@gmail.com>
2024-03-06 22:21:21 +0530

    Added style to the page.

--- app/new-verification/page.tsx
+++ app/new-verification/page.tsx
@@ -1,24 +1,59 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { newVerification } from "@/actions/new-verification";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState, useTransition } from "react";
+import FormContainer from "@/components/auth/formContainer";
+import { Triangle } from "react-loader-spinner";
 const page = () => {
-	const route = useRouter();
+	const [isPending, setTransiton] = useTransition();
+	const [message, setMessage] = useState("");
+	const [messageClass, setMessageClass] = useState("");
 	const params = useSearchParams();
 	const token = params.get("token");
 	const confrimToken = () => {
 		if (!token) return;
 		newVerification(token).then((data) => {
 			if (data?.message === "verified") {
-				route.push("/signIn");
-			} else alert(data.message);
+				setMessage("Your email is verified");
+				setMessageClass("green");
+			} else {
+				setMessageClass("red");
+
+				setMessage(data.message);
+			}
 		});
 	};
 	useEffect(() => {
-		confrimToken();
+		setTransiton(() => {
+			confrimToken();
+		});
 	}, []);
-	return <div>page</div>;
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<FormContainer
+				title="verifying your email"
+				backButtonLabel="Back to login !!"
+				backButtonhref="/signIn"
+			>
+				<div className="pl-[40%]">
+					{isPending ? (
+						<Triangle
+							height="80"
+							width="80"
+							color="#4fa94d"
+							ariaLabel="triangle-loading"
+						/>
+					) : (
+						<div
+							className={`text-xl text-${messageClass}-600 text-center mt-2 ml-[-130px] bg-gray-800 p-6 rounded-xl`}
+						>
+							{message}
+						</div>
+					)}
+				</div>
+			</FormContainer>
+		</div>
+	);
 };
 
 export default page;
