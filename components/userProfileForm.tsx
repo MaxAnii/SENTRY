e8fa0b91b8c7d5b4d52d56bbe37fbb20104fefdd
MaxAnii <ansarbaba2000@gmail.com>
2024-03-07 23:41:20 +0530

    Update user profile function.

--- components/userProfileForm.tsx
+++ components/userProfileForm.tsx
@@ -18,14 +18,15 @@ import { useState, useTransition } from "react";
 import { useCurrentUser } from "@/lib/current-user-session";
 import { updateProfile } from "@/actions/updateProfile";
 import { useSession } from "next-auth/react";
+import ConfrimOTP from "./ConfrimOTP";
 
-type returnData = { error: string };
 const UserProfileForm = () => {
 	const [messsage, setMessage] = useState<String | undefined>("");
 	const user = useCurrentUser();
 	const { update } = useSession();
 	const [isPending, startTransition] = useTransition();
 	if (!user) return;
+	const [showOTP, setShowOTP] = useState<Boolean>(false);
 	const form = useForm<z.infer<typeof userDataSchema>>({
 		resolver: zodResolver(userDataSchema),
 		defaultValues: {
@@ -36,8 +37,14 @@ const UserProfileForm = () => {
 			image: user.image || "",
 		},
 	});
+
 	const onSubmit = (values: z.infer<typeof userDataSchema>) => {
 		setMessage("");
+
+		if (values.phoneNumber !== user.phoneNumber) {
+			setShowOTP(true);
+			return;
+		}
 
 		startTransition(() => {
 			updateProfile(values).then((data) => {
@@ -51,88 +58,91 @@ const UserProfileForm = () => {
 	};
 
 	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-				<FormField
-					control={form.control}
-					name="name"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Name</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="Ex: Ansar"
-									{...field}
-									disabled={isPending}
-								/>
-							</FormControl>
+		<>
+			{!showOTP && <ConfrimOTP></ConfrimOTP>}
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+					<FormField
+						control={form.control}
+						name="name"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Name</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="Ex: Ansar"
+										{...field}
+										disabled={isPending}
+									/>
+								</FormControl>
 
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="email"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="ansar@example.com"
-									{...field}
-									disabled={isPending}
-								/>
-							</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input
+										placeholder="ansar@example.com"
+										{...field}
+										disabled={isPending}
+									/>
+								</FormControl>
 
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="phoneNumber"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Phone Number</FormLabel>
-							<FormControl>
-								<Input type="number" placeholder="xxxxxxxxxx" {...field} />
-							</FormControl>
-							<FormMessage></FormMessage>
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="image"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Image</FormLabel>
-							<FormControl>
-								<Input
-									{...field}
-									type="text"
-									placeholder="https://github.com/shadcn.png"
-									disabled={isPending}
-								/>
-							</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="phoneNumber"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Phone Number</FormLabel>
+								<FormControl>
+									<Input type="number" placeholder="xxxxxxxxxx" {...field} />
+								</FormControl>
+								<FormMessage></FormMessage>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="image"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Image</FormLabel>
+								<FormControl>
+									<Input
+										{...field}
+										type="text"
+										placeholder="https://github.com/shadcn.png"
+										disabled={isPending}
+									/>
+								</FormControl>
 
-							<FormMessage />
-						</FormItem>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<div className="text-red-700">{messsage}</div>
+					{!isPending ? (
+						<Button type="submit" className="w-full">
+							Update
+						</Button>
+					) : (
+						<Button disabled={isPending} className="w-full">
+							Updating...
+						</Button>
 					)}
-				/>
-				<div className="text-red-700">{messsage}</div>
-				{!isPending ? (
-					<Button type="submit" className="w-full">
-						Update
-					</Button>
-				) : (
-					<Button disabled={isPending} className="w-full">
-						Updating...
-					</Button>
-				)}
-			</form>
-		</Form>
+				</form>
+			</Form>
+		</>
 	);
 };
 
