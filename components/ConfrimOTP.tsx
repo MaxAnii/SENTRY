3b950aca23f10da56242504b85a384228f23c781
MaxAnii <ansarbaba2000@gmail.com>
2024-03-07 23:40:40 +0530

    Added a component to enter the OTP.

---
+++ components/ConfrimOTP.tsx
@@ -0,0 +1,28 @@
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+	CardContent,
+} from "./ui/card";
+
+const ConfrimOTP = () => {
+	return (
+		<div className=" ">
+			{/* <div className="  backdrop-blur-3xl pt-10">
+				<Card className="w-[500px]">
+					<CardHeader className="">
+						<CardTitle>Confrim your OTP</CardTitle>
+					</CardHeader>
+
+					<CardContent>
+						<CardDescription>Enter the OTP</CardDescription>
+						<input type="text" />
+					</CardContent>
+				</Card>
+			</div> */}
+		</div>
+	);
+};
+
+export default ConfrimOTP;
