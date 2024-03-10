ab66485bb3b1554c42c5a59033b4ae582fc91c3c
MaxAnii <ansarbaba2000@gmail.com>
2024-03-10 23:48:47 +0530

    Changed the schema of group data.

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,32 +1,32 @@
-import * as z from "zod"
+import * as z from "zod";
 
 export const loginSchema = z.object({
-    email:z.string().email(),
-    password:z.string().min(8)
-})
-
+	email: z.string().email(),
+	password: z.string().min(8),
+});
 
 export const registerSchema = z.object({
-    email:z.string().email(),
-    password:z.string().min(8),
-    confrimPassword:z.string().min(8)
-})
+	email: z.string().email(),
+	password: z.string().min(8),
+	confrimPassword: z.string().min(8),
+});
 
-export const whatsVerificationSchema= z.object({
-    phoneNumber:z.string().min(10)
-})
+export const whatsVerificationSchema = z.object({
+	phoneNumber: z.string().min(10),
+});
 
 export const groupDataSchema = z.object({
-    groupName:z.string().min(1),
-    warning:z.string(),
-    remove:z.string(),
-    toleranceLevel:z.string()
-})
+	groupName: z.string().min(1),
+	warningPerUser: z.string(),
+	removeUser: z.string(),
+	toleranceLevel: z.string(),
+	userId: z.string(),
+});
 
 export const userDataSchema = z.object({
-    id:z.string(),
-    name: z.string(),
-    email:z.string().email(),
-    phoneNumber:z.string().min(10),
-    image:z.string().min(0),
-})
+	id: z.string(),
+	name: z.string(),
+	email: z.string().email(),
+	phoneNumber: z.string().min(10),
+	image: z.string().min(0),
+});
