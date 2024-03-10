bc2e6e45410ca67630188eae24cf6ae41143414f
MaxAnii <ansarbaba2000@gmail.com>
2024-03-10 23:52:47 +0530

    wrapped the children with GroupListProvider.

--- app/(authRoutes)/layout.tsx
+++ app/(authRoutes)/layout.tsx
@@ -1,13 +1,16 @@
 import DashNav from "@/components/dashNav";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import GroupListProvider from "@/hook/GroupListContext";
 const layout = async ({ children }: { children: React.ReactNode }) => {
 	const session = await auth();
 	return (
 		<div>
 			<SessionProvider session={session}>
-				<DashNav></DashNav>
-				{children}
+				<GroupListProvider>
+					<DashNav></DashNav>
+					{children}
+				</GroupListProvider>
 			</SessionProvider>
 		</div>
 	);
