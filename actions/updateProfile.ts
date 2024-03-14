f790cfb73632d39b1fd72dcc0a3c2e43d5077832
MaxAnii <ansarbaba2000@gmail.com>
2024-03-14 23:54:38 +0530

    update phone number seprately.

--- actions/updateProfile.ts
+++ actions/updateProfile.ts
@@ -1,25 +1,21 @@
-"use server"
-import * as z from "zod"
-import { userDataSchema } from "@/schemas"
-import { db } from "@/lib/db"
+"use server";
+import * as z from "zod";
+import { userDataSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
+export const updateProfile = async (values: z.infer<typeof userDataSchema>) => {
+	const validateFields = userDataSchema.safeParse(values);
+	if (validateFields.success) {
+		const { id, name, email, image } = validateFields.data;
+		await db.user.update({
+			where: { id },
+			data: {
+				name,
+				email,
+				image,
+			},
+		});
 
-
-export const updateProfile = async(values:z.infer<typeof userDataSchema>)=>{
-   
-const validateFields = userDataSchema.safeParse(values)
-if(validateFields.success){
-  const{id,name,email,phoneNumber,image} =validateFields.data
-    await db.user.update({
-        where:{id},
-        data:{
-            name,email,phoneNumber,image
-        }
-    })
-  
-        return {message:"Profile updated"}
-    
- 
-}
-
-}
+		return { message: "Profile updated" };
+	}
+};
