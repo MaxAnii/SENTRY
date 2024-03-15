4e38d41e825719570598c067ed0ef499af96cc00
MaxAnii <ansarbaba2000@gmail.com>
2024-03-15 22:06:19 +0530

    Added style and link to signIn page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,31 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import LandingNav from "@/components/landingNav";
+import { useRouter } from "next/navigation";
 export default function Home() {
+	const router = useRouter();
 	return (
 		<>
 			<LandingNav></LandingNav>
 			<div className="flex items-center justify-center h-[90vh] ">
 				<div className="text-center ">
-					<button className="text-green-400 text-l rounded-3xl my-2 p-3   bg-accent ">
+					<button className="dark:text-green-400 text-xl rounded-3xl my-2 p-5 bg-gray-200 dark:bg-accent text-green-600">
 						Welcome to SENTRY
 					</button>
-					<h1 className="text-5xl font-bold">Proctect Your Chat</h1>
-					<div className="text-center p-4 px-[10vw]">
+					<h1 className="text-5xl font-bold">Protect Your Chat</h1>
+					<div className="text-center p-4 px-[10vw] md:mx-[200px] ">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
 						eligendi odio dolore provident, debitis consequatur facilis at?
 						Quisquam magni exercitationem nihil asperiores ipsa officiis maxime
 						sapiente, debitis, amet, at soluta.
 					</div>
-					<div className="text-3xl">
-						<Button variant="outline" size="lg" className="text-xl font-light">
+					<div className="text-3xl ">
+						<Button
+							variant="outline"
+							size="lg"
+							className="text-xl text-indigo-950"
+							onClick={() => router.push("/signIn")}
+						>
 							Get Started
 						</Button>
 					</div>
