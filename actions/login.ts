cb9b3052b8e61bde241fc3d1948f64e46ab08391
MaxAnii <ansarbaba2000@gmail.com>
2024-03-17 23:48:37 +0530

    Formate the code.

--- actions/login.ts
+++ actions/login.ts
@@ -1,52 +1,55 @@
-"use server"
-import { loginSchema } from "@/schemas"
-import * as z from "zod"
-import { db } from "@/lib/db"
-import { signIn } from "@/auth"
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
-import { AuthError } from "next-auth"
-import { generateVerificationToken } from "@/lib/tokens"
-import { sendVerificatonEmail } from "@/lib/mail"
+"use server";
+import { loginSchema } from "@/schemas";
+import * as z from "zod";
+import { db } from "@/lib/db";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificatonEmail } from "@/lib/mail";
 
+export const login = async (values: z.infer<typeof loginSchema>) => {
+	const validateFields = loginSchema.safeParse(values);
+	if (!validateFields.success) return { error: "no match" };
+	const { email, password } = validateFields.data;
+	const existingUser = await db.user.findFirst({
+		where: { email },
+	});
+	if (!existingUser || !existingUser.email || !existingUser.password) {
+		return { error: "Email does not exist!" };
+	}
+	if (!existingUser.emailVerified) {
+		const verficationToken = await generateVerificationToken(
+			existingUser.email
+		);
+		if (verficationToken.email) {
+			await sendVerificatonEmail(
+				verficationToken.email,
+				verficationToken.token
+			);
+		}
+		return { success: "Confrimation Email Sent!" };
+	}
 
-export const login=async(values:z.infer<typeof loginSchema>)=>{
-const validateFields = loginSchema.safeParse(values)
-if(!validateFields.success) return {error:"no match"}
-const {email,password} = validateFields.data;
-const existingUser = await db.user.findFirst({
-    where:{email}
-})
-if(! existingUser || !existingUser.email || !existingUser.password ){
-    return {error:"Email does not exist!"}
-}
-if(!existingUser.emailVerified){
-    const verficationToken = await generateVerificationToken(existingUser.email)
-    if(verficationToken.email){
-        await sendVerificatonEmail(verficationToken.email,verficationToken.token)
-    }
-   return {success:"Confrimation Email Sent!"}
-}
-
-try {
-    await signIn("credentials",{
-        email,password,
-        redirectTo: DEFAULT_LOGIN_REDIRECT 
- 
-    })
-} catch (error) {
-    if (error instanceof AuthError){
-        switch(error.type){
-            case "CredentialsSignin":
-                return {
-                    error:"Invalid credentials"
-                }
-            default:
-                return {
-                    error:"something went wrong"
-                }
-        }
-    }
-    throw error
-}
-
-}
+	try {
+		await signIn("credentials", {
+			email,
+			password,
+			redirectTo: DEFAULT_LOGIN_REDIRECT,
+		});
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case "CredentialsSignin":
+					return {
+						error: "Invalid credentials",
+					};
+				default:
+					return {
+						error: "something went wrong",
+					};
+			}
+		}
+		throw error;
+	}
+};
