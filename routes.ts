0f8c761d647aa5cf3908b7e15a52c2033fd76926
MaxAnii <ansarbaba2000@gmail.com>
2024-03-22 23:47:41 +0530

    Made whatsApp-Auth private route.

--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
-export const publicRoutes=["/"]
+export const publicRoutes = ["/"];
 
-export const authRoutes =["/signIn","/signUp","/whatsAppAuth","/new-verification"]
+export const authRoutes = ["/signIn", "/signUp", "/new-verification"];
 
-export const apiAuthPrefix = "/api/auth"
+export const apiAuthPrefix = "/api/auth";
 
-export const DEFAULT_LOGIN_REDIRECT ="/dashboard"
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
