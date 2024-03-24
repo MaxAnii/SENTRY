923304fb7bbab6fba475af41af9f1a223543f232
MaxAnii <ansarbaba2000@gmail.com>
2024-03-24 12:26:08 +0530

    Moved the DashTable to another component.

--- app/(authRoutes)/dashboard/page.tsx
+++ app/(authRoutes)/dashboard/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import AddNewGroup from "@/components/addNewGroup";
-import DashTable from "@/components/dashTable";
+import GroupData from "@/components/GroupData";
+
 import NoNumberVerifiedAlert from "@/components/NoNumberVerifiedAlert";
 import { useCurrentUser } from "@/hook/current-user-session";
-import { useContext } from "react";
-import { GroupListContext } from "@/hook/GroupListContext";
-import { Triangle } from "react-loader-spinner";
+
+
 const page = () => {
   const user = useCurrentUser();
-  const groupList = useContext(GroupListContext);
-  if (!groupList.data) return;
+
   return (
     <div className="pl-9 ">
       <div className="w-full max-w-[90vw] md:flex">
@@ -25,20 +24,7 @@ const page = () => {
           )}
         </div>
       </div>
-      <div className="my-4 h-[80vh] p-4 shadow-xl sm:mx-[5vw] md:mx-[15vw]">
-        {!groupList.gettingGroupList ? (
-          <DashTable></DashTable>
-        ) : (
-          <div className="flex h-[80vh] items-center justify-center">
-            <Triangle
-              height="80"
-              width="80"
-              color="#4fa94d"
-              ariaLabel="triangle-loading"
-            />
-          </div>
-        )}
-      </div>
+      <GroupData></GroupData>
     </div>
   );
 };
