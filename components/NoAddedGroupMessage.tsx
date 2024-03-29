fa1b6a8daa307dd12ec831ab806a21466cda9931
MaxAnii <ansarbaba2000@gmail.com>
2024-03-29 22:36:42 +0530

    Remove lint error.

--- components/NoAddedGroupMessage.tsx
+++ components/NoAddedGroupMessage.tsx
@@ -11,11 +11,10 @@ const NoAddedGroupMessage = () => {
         </div>
       </div>
       <div className="mt-[-18vh] text-center">
-        <h3 className="text-2xl">{"You don't have any groups added"}</h3>
+        <h3 className="text-2xl">You don&apos;t have any groups added</h3>
         <p className="my-1 text-xl text-gray-500">
-          {
-            "    You currently don't have any groups added, please added some so that you can see the right here"
-          }
+          You currently don&apos;t have any groups added, please added some so
+          that you can see the right here
         </p>
       </div>
     </div>
