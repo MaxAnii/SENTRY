699bc3a956865591f00afcc79018fe619afe4f40
MaxAnii <ansarbaba2000@gmail.com>
2024-04-05 12:40:34 +0530

    UI: Added description.

--- app/(ProtectedRoutes)/dashboard/page.tsx
+++ app/(ProtectedRoutes)/dashboard/page.tsx
@@ -11,8 +11,13 @@ const Page = () => {
   return (
     <div className="w-full">
       <div className="w-full px-5 md:flex lg:px-20">
-        <div className="pb-2 text-3xl">Groups</div>
-        <div className="ml-auto sm:mt-3 md:mt-0">
+        <div>
+          <div className="pb-2 text-3xl">Groups</div>
+          <div className="text-sm  font-light md:text-lg">
+            Here you can see and add a new group.
+          </div>
+        </div>
+        <div className="ml-auto mt-3 md:mt-0">
           {user?.phoneNumber ? (
             <AddNewGroup></AddNewGroup>
           ) : (
