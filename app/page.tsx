d71014946510b4b15b9fdaad3f171d9dacd8c474
MaxAnii <ansarbaba2000@gmail.com>
2024-04-05 12:37:51 +0530

    UI: Centered the welecome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 import AuthLinks from "@/components/authLinks";
 export default function Home() {
   return (
@@ -8,13 +8,13 @@ export default function Home() {
       <Navbar>
         <AuthLinks></AuthLinks>
       </Navbar>
-      <div className="flex w-full items-center justify-center lg:pt-16">
+      <div className="flex h-screen w-full items-center justify-center">
         <div className="text-center">
           <button className="my-2 rounded-3xl bg-gray-200 p-5 text-xl text-green-600 dark:bg-accent dark:text-green-400">
             Welcome to SENTRY
           </button>
           <h1 className="text-3xl font-bold lg:text-5xl">Protect Your Chat</h1>
-          <p className="w-full p-4 text-center lg:px-20">
+          <p className="w-full p-4 text-center lg:px-[120px]">
             Sentry offers a comprehensive solution to enhance user safety on
             digital platforms. By efficiently identifying and filtering out
             semantically toxic or offensive content, the service effectively
