3c6738240f76bbe3ca5575eb761c1bdb81ddbf41
MaxAnii <ansarbaba2000@gmail.com>
2024-04-05 12:41:03 +0530

    UI: Added description

--- app/(ProtectedRoutes)/whatsAppAuth/page.tsx
+++ app/(ProtectedRoutes)/whatsAppAuth/page.tsx
@@ -3,8 +3,12 @@ import WhatAppVerificationForm from "@/components/auth/whatAppVerificationForm";
 const page = () => {
   return (
     <div className="w-full">
-      <div className="w-full pb-2 text-center text-2xl">
-        Verify your whatsApp number
+      <div className="w-full p-6 text-left ">
+        <div className="text-lg md:text-2xl">Verify your whatsApp number</div>
+        <div className="text-sm  font-light md:text-lg">
+          Please confirm your WhatsApp number and add the admin&apos;s number to
+          the group you wish to secure.
+        </div>
       </div>
       <WhatAppVerificationForm></WhatAppVerificationForm>
     </div>
